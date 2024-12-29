8273661074e81ad07a200f62952e5c3107fff1a4
MarkiianPerliak <122627880+MarkiianPerliak@users.noreply.github.com>
2024-12-29 20:34:14 +0200

    commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// 2
+// Напиши функцію countProps(obj), яка рахує кількість властивостей в об'єкті. Функція повертає число — кількість властивостей.
+function countProps(obj) {
+    return Object.keys(obj).length;
+}
+
+const car = {
+    color: 'gray',
+    brand: 'Bently',
+    model: 'MLS 520'
+}
+
+console.log(countProps(car));
+
+// 2
+
+// 3
+
+// 3
